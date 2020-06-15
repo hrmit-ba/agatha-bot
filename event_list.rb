0a82c309718658ad93a2b43c5e3bdc0dcefa03bb
Hrmit <hrmit.twitch@gmail.com>
2020-06-15 12:27:34 +0300

    Agatha v1.1

--- event_list.rb
+++ event_list.rb
@@ -1,5 +1,6 @@
 require_relative 'timeline'
 require 'active_support/all'
+require 'pry'
 
 class EventList
   attr_accessor :event_list
@@ -409,54 +410,310 @@ class EventList
       barb: "Barbarian War"
    }
 
-  def initialize(type)
-    @event_list = []
-    @day = Time.now.utc.wday - 1
-    @time = Time.now.utc.hour/4
+   EVENT_DETAILS = {
+      tiger:  {
+         name: "> Smiledon Attack",
+         tips: "
+               This is a really fun event that requires your focus and time.
 
-    (type == "new" ? NEW_EVENT_LIST : OLD_EVENT_LIST).each do |event|
-      @event_list << ::Timeline.new(event)
-    end
-  end
+               \r\nFor your personal defense have positive morale partners set, preferably 5 star partner, defensive runes, flamen, and defensive boost.
 
-  def current_event
-    current_events = @event_list.select do |event|
-      event.day == @day && event.time == @time
-    end
+               \r\nAlso **VERY IMPORTANT** make sure you don't have lower tier troops at home, it is preffered to have only t4-t5 at home. Lower level tier troops will go to hospital and make you burn early.
 
-    if current_events.size > 1
-      ev_list = current_events.map { |ev| DISPLAY_EVENTS[ev.event_name.to_sym] }.join(", ")
-      return "Current running events are: #{ev_list}."
-    else
-      return "Current running event is #{DISPLAY_EVENTS[current_events.first.event_name.to_sym]}."
-    end
-  end
+               \r\nReinforce the lowest power players in your clan for the biggest points per defense at start. And after wave 18-20 start focusing on getting as many people to wave 25 as possible.
+               ",
 
-  def next_event(event_name)
-    week_start = Time.now.utc.beginning_of_week
+         thumbnail: "https://i.imgur.com/6DFT2mM.png",
+         image: ""
+      },
+      wolf:  {
+         name: "> Wolves",
+         tips: "
+               This event is a great source of **Clan Coins**, and it's broken down in 3 separate events **Plunder**, **Kill Plunderers**, **Horder Protector**.
 
-    upcoming_event = @event_list.select do |event|
-      event.event_name == event_name && (week_start + event.day.days + (event.time*4).hours) > Time.now.utc
-    end
+               \r\nFor **Plunder** make sure you're not using farm talents or farm boost for maximum points per stamina used. I also suggest deciding with the active clans for designated wolfs for each clan/horde to avoid collision.
 
-    if upcoming_event.size > 0
+               \r\nFor **Kill Plunderers** the best way to do it is trade with other horde players 100.000 T1 for recovery pack and maximum rewards. Alternatively you can have an alt in a different horde and kill your own troops.
 
-      event = upcoming_event.first
-      event_start_time = week_start + event.day.days + (event.time*4).hours
+               \r\nFor **Horde Protector** the most common method is killing the wolves then farming them with farming talents and runes, with small amount of troops, the main idea is it takes 2-3 seconds for an attack to register so send enough troops to farm for 2-3 seconds then come back.
+               ",
+
+         thumbnail: "https://i.imgur.com/x2Z1aTd.png",
+         image: ""
+      },
+      wish:  {
+         name: "> Wish Tree",
+         tips: "
+               For this event my only observations is you must not use the free wish tree chances during **Wish Tree** events which do not overlap with **Frost vs Fire** event.
+
+               \r\nAlthough I suggest getting only the first two event rewards with the free wish tree chances you get daily and gems, because they won't cost that much. This usually gets me in top 20 for the event so you might get rank rewards aswell.
+
+               \r\nIf you ever go for rank 1 keep in mind people wait untill last minute to overtake rank.
+
+               \r\nDuring **Frost vs Fire** event preparation there's a quest for 100 wishes being made, and you will have two **Wish Tree** events during that time where you can spend all your free wish tree chances you get from relics. This is also a good time to go for rank as well as it is a lot cheaper to get rank.
+               ",
+
+         thumbnail: "https://i.imgur.com/wuYniNO.png",
+         image: ""
+      },
+      troops: {
+         name: "> Fast Training",
+         tips: "
+                This is a very good event to play because you can get rewards twice once for **FAST TRAINING** and once for **POWER UP** and this is how:
+                \r\nUsing the [Troop Calculator](https://bautils.herokuapp.com/) you can make sure you get all the three rewards without over using speedups.
+
+                \r\nIt's important to save speedups for other events like **BRUTAL SEASON** or **SOLDIER ASSEMBLY**.
+
+                \r\nTo make sure you take full advantage of this event you start training ahead of time (usually during **POWER UP** event) in advance and get all rewards without using any speedups, ofcourse for small accounts this doesn't work the same but you can figure it out.
+                ",
+
+         thumbnail: "https://i.imgur.com/jB06kDQ.png",
+         image: ""
+      },
+      research: {
+         name: "> Research",
+         tips: "
+               There's a lot of dicussion around this event especially since game has added advanced research.
+
+               \r\nNever forget to set your talents to research, and rune set to I personally use **Fine Pottery**, but there are other way to boost the research speed like dragonkin skills, **Laurel** the blue flamen, research boost scrolls, **Sacred Flame** and level of your **Temple of Wisdom**.
+
+               \r\nOne of the first suggestions I have is if you're at the beginning of your journey, and have your paid dragonkin he will help you up with completing research faster. Following this idea when you reach advanced may of the lower level research will be cheap in speedups and can be completed instantly.
+
+               \r\nIt really depends on the play style some people queue researches that don't take long others rush for battle research t4/t5.
+              ",
+
+         thumbnail: "https://i.imgur.com/o5A3bwL.png",
+         image: ""
+      },
+      build: {
+         name: "> Building",
+         tips: "
+               For this event make sure you have your talents set to building, runes set to the most construction speed, I personally use **White Oak**.
+
+               \r\nA few things to add is the $0.99 starter pack is worth buying gives you an extra free 10 minutes to auto complete buildings (very helpful at start).
+
+               \r\nYou can boost you construction speed with dragonkin skills, construction scrolls and **Sacred Flame**.
+
+               \r\nMake sure your clan mates tap that help button it's a huge help in lowering the time on construction.
+              ",
+
+         thumbnail: "https://i.imgur.com/QubyUdH.png",
+         image: ""
+      },
+      rune: {
+         name: "> Whispering of Runes",
+         tips: "
+               For this event it is very important to not waste and expensive fusions. Because during **SVS Battle Preparation** there is a quest that requires you to spend mana by fusing runes.
+
+               \r\nNever go for rank unless you are a spender and rather try and get as close to second/third rewards just by fusing low cost ones.
+
+               \r\nVery important to keep in mind you can summon runes once a day for free (Free re-roll included).
+
+               \r\nNow another important part is how do you get these rune chests for free. This part is simple Events like **Era** , **Altar**, **SVS Battle Prep**, **SVS** will give you rune chests.
+
+               \r\nAnother good way to get them is form **Wild Hunt** or hunting monsters, even during **Wolves** event you get runes every time you kill a wolf.
+              ",
+
+         thumbnail: "https://i.imgur.com/n9zPcsR.png",
+         image: ""
+      },
+      clean: {
+         name: "> Clean Up",
+         tips: "
+              One of the most rewarding events, and all you have to do is to farm.
+
+               \r\nThere are a few tips and tricks here. For one make sure you farm the resources you have the best gather bonus in, this includes runes and talents to be set to farming.
+
+               \r\nTry and farm level 6 and 5 resources preferably on your own color. Make sure you have territory gathering boost set in runes for extra points.
+
+               \r\nFor ranking you will need to prepare for this event and make sure you get all your marches + extra march on high level resources to end soon after **Clean UP** starts, this will give you a nice boost.
+
+               \r\nGet your clan mates to help by donating almost completed resources or high level ones to you.
+              ",
+
+         thumbnail: "https://i.imgur.com/Q7yXYia.png",
+         image: ""
+      },
+      power: {
+         name: "> Power UP",
+         tips: "
+               **Power UP** is one of the most easiest events to get the 3 rewards and this is how:
+
+               \r\nYou can for example complete training of troops during **Fast Training** and then just take the troops out during this event.
+
+               \r\nYou can level your dragonkin, level up your account by hording experience boosts or even and this is for SVS fighters, use **Revive Gate** to ressurect your dead troops.
+              ",
+
+         thumbnail: "https://i.imgur.com/4UrtqaJ.png",
+         image: ""
+      },
+      barb: {
+         name: "> Barbarian war",
+         tips: "
+               This event is a risky one if you're not the power house of your server or don't have enough to defend yourself from rallies and full dragonkin skills attacks.
+
+               \r\nIf you just want to survive use 4x8h bubble if you are not online at the start of event. Alternatively you can hide under another player's bubble if he has space and he has shared his timer in a chat group. Never use bubbles that are not shared they might pop at any time.
+
+               \r\nIf you are going for rewards/rank there are multiple things you can do:
+
+                  \r\n1. Make sure you're on full battle talents/runes/flamen/boosts and you have your best team set on defensive. ( use of antiscout is highly recomanded)
+                  \r\n2. Use revive to ressurect dead clan mates troops and kill them off for free points ( multiple ressurects used more points)
+                  \r\n3. If you find a big target organize a rally with your clan members ( use of dragonkin skills and advanced research **Set Off** is highly recomanded)
+              ",
+
+         thumbnail: "https://i.imgur.com/NLPuTxx.png",
+         image: ""
+      }
+   }
+
+   LONG_EVENTS = {
+      tiger: {
+         new: {
+            start: { day: 3, time: 0 },
+            end: { day: 3, time: 5 }
+         },
+         old: {
+            start: { day: 3, time: 0 },
+            end: { day: 3, time: 5 }
+         }
+      },
+      wolf: {
+         new: {
+            start: { day: 3, time: 0 },
+            end: { day: 3, time: 1 }
+         },
+         old: {
+            start: { day: 3, time: 4 },
+            end: { day: 3, time: 5 }
+         }
+
+      },
+      clean: {
+         new: {
+            start: { day: 6, time: 5 },
+            end: { day: 0, time:0 }
+         },
+         old: {
+            start: { day: 6, time: 5 },
+            end: { day: 0, time:0 }
+         }
+      },
+      barb: {
+         new: {
+            start: { day: 5, time: 0 },
+            end: { day: 5, time: 5 }
+         },
+         old: {
+            start: { day: 5, time: 0 },
+            end: { day: 5, time: 5 }
+         }
+      }
+   }
+
+   def initialize(type)
+      @event_list = []
+      w_day = Time.now.utc.wday - 1
+      @day = w_day < 0 ? 6 : w_day
+      @time = Time.now.utc.hour/4
+      @week_start = Time.now.utc.beginning_of_week
+      @type = type == "new" ? :new : :old
+
+      (type == "new" ? NEW_EVENT_LIST : OLD_EVENT_LIST).each do |event|
+         @event_list << ::Timeline.new(event)
+      end
+   end
+
+   def get_event_detail(event_name)
+      details = {}
+
+      current_events = get_current_event
+      next_event = get_next_event(event_name)
+
+      case event_name
+      when 'troops'   then details = EVENT_DETAILS[:troops]
+      when 'wish'     then details = EVENT_DETAILS[:wish]
+      when 'rune'     then details = EVENT_DETAILS[:rune]
+      when 'power'    then details = EVENT_DETAILS[:power]
+      when 'clean'    then details = EVENT_DETAILS[:clean]
+      when 'research' then details = EVENT_DETAILS[:research]
+      when 'build'    then details = EVENT_DETAILS[:build]
+      when 'wolf'     then details = EVENT_DETAILS[:wolf]
+      when 'tiger'    then details = EVENT_DETAILS[:tiger]
+      when 'barb'     then details = EVENT_DETAILS[:barb]
+      else {}
+      end
+
+      details
+   end
+
+   def get_current_event
+      current_events = @event_list.select do |event|
+         event.day == @day && event.time == @time
+      end
+   end
+
+   def get_next_event(event_name)
+      @week_start = Time.now.utc.beginning_of_week
+
+      upcoming_events = @event_list.select do |event|
+         event.event_name == event_name && (@week_start + event.day.days + (event.time*4).hours) > Time.now.utc
+      end
+
+      if upcoming_events.size > 0
+         event = upcoming_events.first
+      else
+         @week_start += 7.days
+         event = @event_list.select do |event|
+            event.event_name == event_name && (@week_start + event.day.days + (event.time*4).hours) > Time.now.utc
+         end.first
+      end
+   end
+
+   def get_time_to_start(event)
+      event_start_time = @week_start + event.day.days + (event.time*4).hours
       duration = event_start_time - Time.now.utc
+   end
 
-      "Next #{DISPLAY_EVENTS[event.event_name.to_sym]} will start in #{get_formatted_date(duration)}."
-    else
-      week_start += 7.days
-      event = @event_list.select do |event|
-        event.event_name == event_name && (week_start + event.day.days + (event.time*4).hours) > Time.now.utc
-      end.first
+   def get_remaining_time(event)
+      @week_start = Time.now.utc.beginning_of_week
 
-      event_start_time = week_start + event.day.days + (event.time*4).hours
-      duration = event_start_time - Time.now.utc
-      "Next #{DISPLAY_EVENTS[event.event_name.to_sym]} will start in #{get_formatted_date(duration)}."
-    end
-  end
+      event_name = event.event_name.to_sym
+
+      is_long_ev = LONG_EVENTS[event_name].present? && (
+            LONG_EVENTS[event_name][@type][:start][:day] == event.day ||
+            LONG_EVENTS[event_name][@type][:end][:day] == event.day
+         )
+
+      if is_long_ev
+         event_end_time = @week_start + LONG_EVENTS[event_name][@type][:end][:day].days + ((LONG_EVENTS[event_name][@type][:end][:time]+1)*4).hours
+      else
+         event_end_time = @week_start + event.day.days + ((event.time+1)*4).hours
+      end
+
+      return event_end_time - Time.now.utc
+   end
+
+   def current_event
+      current_events = get_current_event
+      details = []
+
+      current_events.each do |event|
+         detail = get_event_detail(event.event_name)
+         detail[:ends_in] = "Event ends in: **#{get_formatted_date(get_remaining_time(event))}**"
+
+         details << detail
+      end
+
+      return details
+   end
+
+   def next_event(event_name)
+      event = get_next_event(event_name)
+      details = get_event_detail(event.event_name)
+      details[:starts_in] = "Event starts in: **#{get_formatted_date(get_time_to_start(event))}**"
+
+      details
+   end
 
   def get_formatted_date(time)
     final_string  = ""
--- init.rb
+++ init.rb
@@ -1,10 +1,11 @@
 require 'discordrb'
 require_relative 'event_list'
 require 'dotenv/load'
+require 'pry'
 
 @agatha_bot = Discordrb::Commands::CommandBot.new(
   token: ENV["TOKEN"],
-  client_id: ENV["C_ID"], prefix: '!',
+  client_id: ENV["C_ID"], prefix: '?',
   help_command: :comlist,
   ignore_bots: true
 )
@@ -14,23 +15,30 @@ require 'dotenv/load'
     embed.colour = 0xFF8000
     embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: 'https://i.imgur.com/pnTz7VH.png')
     embed.description ="
-    > **!comlist**
+    > **?comlist**
     Lists all commands.
 
-    > **!who_made_you?**
+    > **?who_made_you?**
     Will tell you who's the author of this bot.
 
-    > **!coffee_please**
+    > **?coffee_please**
     Agatha will bring you coffee.
 
-    > **!hello**
+    > **?hello**
     Agatha will say hello to you (not alone anymore).
 
-    > **!current_event [new/old]**
+    > **?fusion_guide**
+    Link to Rain's fusion guide
+
+    > **?burn [HP]**
+    Use this command so that Agatha tells you how many times you need to attack an outpost to completely burn
+    Example: **?burn 21000**
+
+    > **?current_event [new/old]**
     Will tell you what is the current ongoing daily event.
     You can specify old or new based on how old your server is.
 
-    > **!next [event] [new/old]**
+    > **?next [event] [new/old]**
     You can specify old or new based on how old your server is.
 
     The events you can choose from are:
@@ -45,11 +53,8 @@ require 'dotenv/load'
       - **clean**    [Clean Up]
       - **barb**     [Barbarian War]
 
-    Example: **!next power new**
+    Example: **?next power new**
     "
-
-    #embed.add_field(name: "> !who_made_you?", value:"Who made this bot", inline: false)
-    #embed.add_field(name: "> !coffee_please", value: "Agatha gets you coffee :)", inline: false)
   end
 end
 
@@ -65,13 +70,6 @@ end
   event.user.pm "Hello chief #{event.user.mention} how can I help you today?"
 end
 
-@agatha_bot.command(:current_event) do |event|
-  type = event.message.content.split(' ')[1].downcase rescue "old"
-
-  event_list = EventList.new type
-  event.user.pm "#{event_list.current_event} Anything else I can help you with chief #{event.user.mention}?"
-end
-
 @agatha_bot.command(:next) do |event|
   params = event.message.content.split(' ')
   return unless params.size > 1
@@ -79,14 +77,58 @@ end
   type = params[2].downcase rescue "old"
 
   event_list = EventList.new type
-  event.user.pm "#{event_list.next_event(params[1])} Anything else I can help you with chief #{event.user.mention}?"
+  details = event_list.next_event(params[1])
+
+  event.user.pm.send_embed do |embed|
+    embed.colour = 0xFF4000
+    embed.title = details[:name]
+    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: details[:thumbnail])
+    embed.add_field(name: 'Time until', value: details[:starts_in], inline: false)
+    embed.add_field(name: "> Tips", value: details[:tips], inline: false)
+  end
 end
 
-@agatha_bot.command(:schedule) do |event|
-  event.channel.send_embed do |embed|
-    embed.image = Discordrb::Webhooks::EmbedImage.new(url: 'https://i.imgur.com/TP0BKka.png')
-    embed.add_field(name: "Current Event", value:"Power UP - 00:32:12 Remaining", inline: false)
-    embed.add_field(name: "Tips", value: "Use https://bautils.herokuapp.com/ to calculate minimum required troop to get all 3 rewards.", inline: false)
+@agatha_bot.command(:current_event) do |event|
+  type = event.message.content.split(' ')[1].downcase rescue "old"
+
+  event_list = EventList.new type
+  details = event_list.current_event
+  details_count = details.size
+
+  event.user.pm.send_embed do |embed|
+    embed.colour = 0xFF4000
+    embed.title = details[0][:name]
+    embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: details[0][:thumbnail])
+    embed.add_field(name: 'Time remaining', value: details[0][:ends_in], inline: false)
+    embed.add_field(name: "> Tips", value: details[0][:tips], inline: false)
+  end
+
+  if details_count > 1
+    event.user.pm.send_embed do |embed|
+      embed.colour = 0xFF8000
+      embed.title = details[1][:name]
+      embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: details[1][:thumbnail])
+      embed.add_field(name: 'Time remaining', value: details[1][:ends_in], inline: false)
+      embed.add_field(name: "> Tips", value: details[1][:tips], inline: false)
+    end
+  end
+end
+
+@agatha_bot.command(:burn) do |event|
+  params = event.message.content.split(' ')
+  return unless params.size > 1
+  if params[1].to_i == 0
+    event.user.pm "Hello chief, please provide a valid outpost HP as number for example: ?burn 25000"
+  else
+    event.user.pm "Hello chief, you need to attack tha outpost #{(params[1].to_i/1800.0).ceil}"
+  end
+end
+
+@agatha_bot.command(:fusion_guide) do |event|
+  event.user.pm.send_embed do |embed|
+    embed.colour = 0xFF8000
+    embed.title = "Rune fusion quantity requirements"
+    embed.image = Discordrb::Webhooks::EmbedImage.new(url: 'https://i.imgur.com/xmzDspf.jpg')
   end
 end
 
